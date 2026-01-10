42b3ec84c2c4ceeda880ff7d96645dc79280b065
Aoto Miyanaka <aoto.miyanaka@gmail.com>
2026-01-10 15:29:53 -0800

    Initial Commit

---
+++ LineOfDeath/LineOfDeathApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineOfDeathApp.swift
+//  LineOfDeath
+//
+//  Created by Aoto on 2026/01/10.
+//
+
+import SwiftUI
+
+@main
+struct LineOfDeathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
